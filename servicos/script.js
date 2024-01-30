f5155f56bf814bf9a1ac5f022bd2b6fc6ec5baff
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-01-30 02:01:42 -0300

    upgrade

---
+++ servicos/script.js
@@ -0,0 +1,21 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/iconemenu.svg"
+    } else{
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "img/iconecancel.svg"
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var mobileMenuButton = document.querySelector(".mobile-menu-icon button");
+    var body = document.body;
+
+    mobileMenuButton.addEventListener("click", function () {
+        body.classList.toggle("menu-opened");
+    });
+});
+
+
