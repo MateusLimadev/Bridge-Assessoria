4867d97d3e02bf5100ce605088e684f38b1b2d86
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-01 18:03:37 -0300

    update

--- bridgehome.js
+++ bridgehome.js
@@ -2,10 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/iconemenu.svg"
+        document.querySelector('.icon').src = "imagens/iconemenu.svg"
     } else{
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "img/iconecancel.svg"
+        document.querySelector('.icon').src = "imagens/iconecancel.svg"
     }
 }
 
@@ -17,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
         body.classList.toggle("menu-opened");
     });
 });
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -38,3 +37,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+// Ajusta a largura dos cards com base no tamanho da janela de visualização
+function ajustarLarguraCards() {
+    const cardsContainer = document.getElementById('cardscontainer');
+    const cards = cardsContainer.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.style.maxWidth = `${window.innerWidth / 4}px`; // Dividindo por 4 para mostrar inicialmente 4 cards
+    });
+}
+
+// Executa a função ao carregar a página e ao redimensionar a janela
+window.addEventListener('load', ajustarLarguraCards);
+window.addEventListener('resize', ajustarLarguraCards);
