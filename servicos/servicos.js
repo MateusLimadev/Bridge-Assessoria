76a2998f8b6c046ac017c01ee74d84edc6eaa580
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2024-02-24 19:21:18 -0300

    Update servicos.js

--- servicos/servicos.js
+++ servicos/servicos.js
@@ -1,6 +1,6 @@
 const btnmobile = document.getElementById('btn-mobile');
 
-// Adiciona a classe 'transparent' inicialmente
+
 var header = document.getElementById('header');
 header.classList.add('transparent');
 
@@ -24,7 +24,7 @@ btnmobile.addEventListener('touchstart', toggleMenu);
 window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY;
 
-    // Adiciona ou remove a classe 'transparent' com base no scroll
+    
     if (scrollPosition > 0) {
         header.classList.remove('transparent');
     } else {
@@ -34,23 +34,23 @@ window.addEventListener('scroll', function () {
 
 
 
-// Obtenha o elemento alvo
+
 const elementoCartoes = document.querySelector('.cards');
 
-// Configure o Intersection Observer
+
 const observador = new IntersectionObserver(entradas => {
     entradas.forEach(entrada => {
         if (entrada.isIntersecting) {
-            // Adicione a classe 'show' quando o elemento alvo estiver visível
+           
             elementoCartoes.classList.add('show');
         } else {
-            // Remova a classe 'show' quando o elemento alvo estiver fora de vista
+            
             elementoCartoes.classList.remove('show');
         }
     });
-}, { threshold: 0.3 }); // Ajuste o limiar conforme necessário
+}, { threshold: 0.3 }); 
 
-// Comece a observar o elemento alvo
+
 observador.observe(elementoCartoes);
 
 
@@ -80,6 +80,6 @@ document.addEventListener('DOMContentLoaded', function () {
             document.addEventListener('scroll', handleScroll);
             window.addEventListener('resize', handleScroll);
 
-            // Chame handleScroll() ao carregar a página para verificar os cards visíveis inicialmente
+            
             handleScroll();
         });
